8ca0993b3247e1d110eac24c8b62509debe08cf8
Wasim <wasimrandhawa@gmail.com>
2016-06-16 08:50:25 +0200

    New Project

---
+++ GymPassBokning/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GymPassBokning.Startup))]
+namespace GymPassBokning
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
